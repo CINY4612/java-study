6ad9dff735605f2e04508d79ac15b7ccd2b5ff1f
CINY4612 <ciy5364612@gmail.com>
2023-02-02 00:42:54 +0900

    practice. cmd read practice related to issue #4

--- src/practice/CmdPractice.java
+++ src/practice/CmdPractice.java
@@ -1,17 +1,45 @@
 package practice;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class CmdPractice {
+
     public static void main(String[] args) {
+        CmdPractice cmdPractice = new CmdPractice();
+
         try{
-//            String cmd = "c: && hello.txt";
-//            String cmd = "c: && cd Users/user && hello.txt";
-            String cmd = "c: && cd/ && cd Users/user && hello.txt";
-            Runtime.getRuntime().exec("cmd /c " + cmd);
+            String cmd = "help";
+            Process process = Runtime.getRuntime().exec("cmd /c " + cmd);
+
+            String result = cmdPractice.readCmdResult(process);
+            System.out.println(result);
         } catch (IOException e){
             System.out.println("ERROR!!!");
             System.out.println(e.getMessage());
         }
+    }
+
+    public String readCmdResult(Process process) throws IOException {
+        // cmd 결과값이 담긴 process를 문자 단위로 읽어들임
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream(), "MS949"));
+        String line = null;
+
+        // 문자열 추가 자료형
+        StringBuffer sb = new StringBuffer();
+
+        try{
+            while((line = reader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        } catch (IOException e){
+            System.out.println("ERROR!!!");
+            System.out.println(e.getMessage());
+        }
+
+        return sb.toString();
     }
 }
